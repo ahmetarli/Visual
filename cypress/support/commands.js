bb859eb1ed098b9df06eb693066ac14a8c538568
ahmetarli <sadikahmetarli@gmail.com>
2025-09-16 23:41:42 +0300

    dd1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit("https://www.edu.goit.global/account/login")
+    cy.get('#user_email').type(email)
+    cy.get("#user_password").type(password)
+    cy.get("[type='submit']").click()
+})
